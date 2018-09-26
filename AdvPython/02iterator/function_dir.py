8bbe606d0c6073060bf8c6f92e260420fb6b5dc4
QiangAI <38395870@qq.com>
2018-09-26 22:45:45 +0800

    yangqiang

--- AdvPython/02iterator/function_dir.py
+++ AdvPython/02iterator/function_dir.py
@@ -1,2 +1,13 @@
 #coding=utf-8
+#可循环对象
+str="Louis Young"   #字符串
+lst=[1,2,3,4]       #列表
+tpl=(1,2,3,4)       #元组
+fle=open("data.txt")    #文件
 
+print(dir(str))
+print(dir(lst))
+print(dir(tpl))
+print(dir(fle))
+
+#可循环对象，都有一个—__iter__方法
